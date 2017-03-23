7491ebf3e729d88d293f698df39eddc581f50f33
zmcNotafraid <mingchang555@hotmail.com>
2017-03-24 05:14:21 +0800

    now can set name color by role

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,5 +1,6 @@
 defmodule Chat.PageController do
   use Chat.Web, :controller
+  require Logger
 
   def index(conn, %{"xtoken" => jwt}) do
     if jwt != String.trim("") do
@@ -7,10 +8,15 @@ defmodule Chat.PageController do
       {:ok, claims} = JsonWebToken.verify(jwt, %{key: Application.get_env(:chat, Chat.Endpoint)[:private_key]})
       enusername = Base.encode64(claims[:iss])
       Redis.command(~w(SET #{csrf} #{enusername}))
+      {:ok, role} = Redis.command(~w(GET #{claims[:sub]}:role))
+      if role == nil || (Redis.command(~w(TTL #{claims[:sub]}:role)) < 1) do
+        role = ""
+      end
+      Logger.info(role)
       {:ok, deusername} = Base.decode64(enusername)
-      render conn, "index.html", userid: claims[:jti], username: deusername, usersub: claims[:sub], adi: claims[:adi], tag: claims[:tag], csrf: csrf
+      render conn, "index.html", userid: claims[:jti], username: deusername, usersub: claims[:sub], adi: claims[:adi], tag: claims[:tag], csrf: csrf, role: role
     else
-      render conn, "index.html", userid: "", username: "", usersub: "", adi: "", tag: "", csrf: ""
+      render conn, "index.html", userid: "", username: "", usersub: "", adi: "", tag: "", csrf: "", role: ""
     end
   end
 
--- web/static/js/app.js
+++ web/static/js/app.js
@@ -12,6 +12,7 @@ class App {
     var $adi       = $("#adi")
     var $tag       = $("#tag")
     var $csrf      = $("#csrf")
+    var $role      = $("#role")
 
     let socket = new Socket("/socket", {
       logger: ((kind, msg, data) => { console.log(`${kind}: ${msg}`, data) })
@@ -31,7 +32,7 @@ class App {
     $input.off("keypress").on("keypress", e => {
       if (e.keyCode == 13) {
         if ($.trim($input.val()).length > 0) {
-          chan.push("new:msg", {csrf: $csrf.val(), userid: $userid.val(), user: $username.val(), sub: $usersub.val(), adi: $adi.val(), body: $input.val(), tag: $tag.val()})
+          chan.push("new:msg", {csrf: $csrf.val(), userid: $userid.val(), user: $username.val(), sub: $usersub.val(), adi: $adi.val(), body: $input.val(), tag: $tag.val(), role: $role.val()})
           $input.val("")
         }
       }
@@ -40,11 +41,15 @@ class App {
     chan.on("history:msgs", msgs => {
       for (var i = 0; msgs.history.length > i; i++) {
         var logs = decodeURIComponent(escape(window.atob( msgs.history[i] )))
+        console.info("=============")
+        console.info(logs)
         var msg = {user : logs.split("~")[1],
                   sub : logs.split("~")[2], 
                   adi : logs.split("~")[3],
                   body : logs.split("~")[4],
-                  tag: logs.split("~")[5]
+                  tag: logs.split("~")[5],
+                  role: logs.split("~")[6]
+
         }
         $messages.append(this.messageTemplate(msg))
         scrollTo(0, document.body.scrollHeight)
@@ -68,12 +73,22 @@ class App {
     let adi      = this.sanitize(msg.adi || "false")
     let body     = this.sanitize(msg.body)
     let tag      = this.sanitize(msg.tag)
+    let role      = this.sanitize(msg.role)
     if (username == "SYSTEM") {
       return(`<p class="text-center"><span class="time">${moment(body * 1000).fromNow()}</span></p>`)
     }
+    console.info(role == "normal_believer")
     if (adi == "true") {
       return(`<p><span class="${adi}">${username}</span> <span class="${tag}">&nbsp;</span> ${body}</p>`)
-    } else {
+    } else if (role == "normal_believer") {
+      return(`<p><span class="${adi}">${username}</span><span class="normal believer">#${usersub}</span> <span class="${tag}">&nbsp;</span> ${body}</p>`)
+    }
+    else if (role == "advanced_believer") {
+      return(`<p><span class="${adi}">${username}</span><span class="advanced believer">#${usersub}</span> <span class="${tag}">&nbsp;</span> ${body}</p>`)
+    }
+    else if (role == "true_name") {
+      return(`<p><span class="${adi}">${username}</span><span class="true name">#${usersub}</span> <span class="${tag}">&nbsp;</span> ${body}</p>`)
+    }else{
       return(`<p><span class="${adi}">${username}</span><span class="sub">#${usersub}</span> <span class="${tag}">&nbsp;</span> ${body}</p>`)
     }
   }
